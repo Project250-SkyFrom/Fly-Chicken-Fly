8da60f7cf23d6ab8b7160068fb8a4246b7d2b7ea
Jimmy <zhijianliu72@gmail.com>
2024-04-02 15:13:27 -0600

    bug fix

--- Assets/Scripts/VerticalPlatformMovement.cs
+++ Assets/Scripts/VerticalPlatformMovement.cs
@@ -5,13 +5,14 @@ public class VerticalPlatformMovement : MonoBehaviour
     public float speed = 2f;
     public float upBound = 3f;    // Set the upper boundary for the platform
     public float downBound = -3f; // Set the lower boundary for the platform
+    public GameObject parent;
 
     private float startY;           // Initial y-position of the platform
 
     void Start()
     {
         // Store the initial y-position of the platform
-        startY = transform.position.y;
+        startY = parent.transform.position.y;
     }
 
     void Update()
