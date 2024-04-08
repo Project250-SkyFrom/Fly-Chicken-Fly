5ca018de76a3c3b58d8ec8c4a261a109aae0eafe
Fardeen Faisal <68156295+fardeenfs@users.noreply.github.com>
2024-04-08 14:58:41 -0600

    lump when hitting wall (#162)

--- Assets/Scripts/BoundaryController.cs
+++ Assets/Scripts/BoundaryController.cs
@@ -64,4 +64,16 @@ public class BoundaryController : MonoBehaviour
         }
     }
 
+    private void OnCollisionEnter2D(Collision2D collision){
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Wall Hit!");
+            if(DataManager.Instance.currentGameMode == "death"){
+                EventController.Instance.AddLump();
+                AudioController.Instance.PlayChickenHit();
+            }
+            
+        }
+    }
+
 }
